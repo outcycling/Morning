4902f4ec79eeae713072411825b28f241ecf58dd
outcycling <graham@outcycling.org>
2017-08-22 16:36:32 -0400

    user photos table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170821201755) do
+ActiveRecord::Schema.define(version: 20170822195939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "photos", force: :cascade do |t|
+    t.integer  "user_id"
+    t.boolean  "main"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
