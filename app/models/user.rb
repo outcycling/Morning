40ff3a15e9db9a4bebcd791d2f09107611f6d697
danielfsokol <danielfsokol@gmail.com>
2017-08-22 16:27:38 -0400

    added profile table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  has_one :profile, :dependent => :destroy,
+  primary_key: :id,
+  foreign_key: :user_id,
+  class_name: 'Profile'
+
+
 end
